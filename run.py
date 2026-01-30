384666d3ba7bbc1760a888586470c01af5c548b7
AlexMorgan3817 <Ivan.zvereff@gmail.com>
2026-01-30 09:18:22 +0700

    Debug flag for run.py

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 import uvicorn
+import argparse
+
+args = argparse.ArgumentParser()
+args.add_argument("--debug", action="store_true")
+
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
+    args = args.parse_args()
+    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=args.debug)
